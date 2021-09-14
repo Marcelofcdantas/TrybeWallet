3fae8790cd450299a20ad06914b76e60a179c39d
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-09-14 18:24:11 -0300

    Updates

---
+++ precache-manifest.1bc5eb4fdd999b7877fa372e909d3489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26df6e65b216c942e5d5a4db789ba2b8",
+    "url": "/TrybeWallet/index.html"
+  },
+  {
+    "revision": "e07625dbd8e07710f82a",
+    "url": "/TrybeWallet/static/css/main.3615a680.chunk.css"
+  },
+  {
+    "revision": "4ecd4608f7e52f3834cf",
+    "url": "/TrybeWallet/static/js/2.82f1be24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TrybeWallet/static/js/2.82f1be24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07625dbd8e07710f82a",
+    "url": "/TrybeWallet/static/js/main.c9a33139.chunk.js"
+  },
+  {
+    "revision": "7064498208332b0843ee",
+    "url": "/TrybeWallet/static/js/runtime-main.e31e136f.js"
+  },
+  {
+    "revision": "b7e5e47094f465fb7410e66ff382e294",
+    "url": "/TrybeWallet/static/media/money.b7e5e470.jpg"
+  }
+]);
